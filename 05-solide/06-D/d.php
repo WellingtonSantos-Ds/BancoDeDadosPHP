4b8a70b2b2062a8d21071089e5f8423ed43d5521
WellingtonSantos-Ds <xispakix@gmail.com>
2024-01-20 17:26:00 -0300

    up

--- 05-solide/06-D/d.php
+++ 05-solide/06-D/d.php
@@ -1,7 +1,7 @@
 <?php 
 // Dependency inversion principle (Principio da inversão de dependência)
 
-class MsqlConn
+class MysqlConn
 {
   public function conexao(){}
 }
@@ -13,6 +13,68 @@ class UsuarioDao
 
   public function __construct()
   {
-    $this->db = new MsqlConn();
+    $this->db = new MysqlConn();
   }
 }
+
+
+// também esta errado
+
+class MysqlConn1
+{
+  public function conexao(){}
+}
+
+// Desse jeito  estou fazendo apenas uma injeção de dependência 
+class UsuarioDao1
+{
+  private $db;
+
+  public function __construct(MysqlConn1 $conn)
+  {
+    $this->db = $conn;
+  }
+
+}
+
+
+
+// Abstração para a conexão com o banco de dados
+interface DbConnection
+{
+  public function conexao();
+}
+
+// Implementação concreta da conexão com o banco de dados
+class MsqlConn2 implements DbConnection
+{
+  public function conexao()
+  {
+      // Implementação da conexão MySQL
+  }
+
+}
+
+class OracleConn implements DbConnection
+{
+  public function conexao()
+  {
+      // Implementação da conexão MySQL
+  }
+}
+
+// Classe UsuarioDao agora depende da abstração (DbConnection)
+class UsuarioDao2
+{
+  private $db;
+  // O construtor só aceita o banco de dados que tiver implementado o DbConnection
+  public function __construct(DbConnection $db)
+  {
+      $this->db = $db;
+  }
+}
+$dbConn = new MsqlConn2(); 
+$dbConn2 = new OracleConn(); 
+
+$usuarioDao = new UsuarioDao2($dbConn);
+$usuarioDao2 = new UsuarioDao2($dbConn);
